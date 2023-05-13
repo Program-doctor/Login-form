f800bce31f3035ec7a568d699f5d49607ebdef51
PromiseSyl <104115299+Program-doctor@users.noreply.github.com>
2023-05-13 15:23:17 +0100

    HTML/CSS Login form
    Done with the HTML/CSS of the login form

---
+++ index.js
@@ -0,0 +1,10 @@
+let login = document.querySelector('.login');
+let create = document.querySelector('.create');
+let container = document.querySelector('.container');
+login.onclick= function(){
+    container.classList.add('signinform');
+}
+();
+create.onclick = function(){
+    container.classList.remove('signinform');
+}
